e9c87ed3cb404492568a2b4e3e60a3817d33c86d
Irene <ire.andreina.z@gmail.com>
2022-02-17 15:09:43 +0100

    add structure of game function

---
+++ assets/JS/function.js
@@ -0,0 +1,56 @@
+//runGame()
+
+//checkAnswer()
+
+//calculateCorrectAnswer()
+
+//incrementScore()
+
+//incrementWrongAnswer()
+
+//displayAdditionQuesion()
+
+//displaySubtractQuestion()
+
+//displayMultiplyQuestion()
+
+
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function calculateCorrectAnswer() {
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuesion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+
+    
+}
