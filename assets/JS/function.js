dbfd726d174959603f66e776e654d619a06834b8
Irene <ire.andreina.z@gmail.com>
2022-02-17 15:28:49 +0100

    add events to the game function

--- assets/JS/function.js
+++ assets/JS/function.js
@@ -1,19 +1,20 @@
-//runGame()
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listener to them
 
-//checkAnswer()
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-//calculateCorrectAnswer()
-
-//incrementScore()
-
-//incrementWrongAnswer()
-
-//displayAdditionQuesion()
-
-//displaySubtractQuestion()
-
-//displayMultiplyQuestion()
-
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
 
 function runGame() {
 
@@ -52,5 +53,5 @@ function displaySubtractQuestion() {
 
 function displayMultiplyQuestion() {
 
-    
+
 }
